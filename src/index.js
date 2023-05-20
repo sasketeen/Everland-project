1a9427b57700d4b35b6bba0b936c41e48806d644
Igor Kashapov <grkasapov@gmail.com>
2023-05-20 14:11:51 +0500

    #18 #19 #41

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 import "./pages/index.scss"
+import Form from "./components/Form";
+import { donateForm, charityForm, amountInputs, hiddenTextInput } from "./utils/constants";
+
+
+const donateFormElement = new Form(donateForm, donateFormSubmit, donateFormListeners);
+donateFormElement.setEventListeners();
+
+const charityFormElement = new Form(charityForm, charityFormSubmit);
+charityFormElement.setEventListeners();
+
+function donateFormSubmit(inputValues) {
+	console.log(inputValues)
+}
+
+function donateFormListeners() {
+	amountInputs.forEach(item => {
+		item.addEventListener('click', (evt) => {
+			toggleSumInputVisibility(evt.target)
+		})
+	})
+}
+
+function charityFormSubmit(inputValues) {
+	amountInputs.forEach(item => {
+		if (inputValues.sum === item.value) {
+			item.checked = true;
+			toggleSumInputVisibility(item);
+		}
+	})
+}
+
+function toggleSumInputVisibility(item) {
+	if (item.value === 'other') {
+		hiddenTextInput.classList.remove('form__input_type_text-hidden');
+		hiddenTextInput.required = true;
+	} else {
+		hiddenTextInput.classList.add('form__input_type_text-hidden');
+		hiddenTextInput.required = false;
+	}
+}
+
+
+
+
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const donateForm = document.querySelector('.donate-form');
+export const charityForm = document.querySelector('.charity__form');
+const amountInputContainer = donateForm.querySelector('.donate-form__amount-input-container');
+export const amountInputs = amountInputContainer.querySelectorAll('.form__input');
+export const hiddenTextInput = document.querySelector('.form__input_type_text-hidden');
